a042fc1147670aeaf4f711ceddabb8a658611bdd
YushiroDodo63 <71711872+YushiroDodo63@users.noreply.github.com>
2021-06-30 00:42:39 +0900

    not commit this file

--- api/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        skip_before_action :verify_authenticity_token, if: :devise_controller? # APIではCSRFチェックをしない
-end
-      
