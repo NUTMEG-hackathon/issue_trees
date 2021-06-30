40248d7f11934c1aa7e9591da0a784ecb2ca614c
ryuseifujisaki <fujiryu1122@gmail.com>
2021-06-30 17:18:42 +0900

    login 途中経過

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,5 +1,9 @@
-class User < ApplicationRecord
-  has_many :issues
-  has_many :project_users
-  has_many :user_skills
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :project_users
   resources :user_skills
   resources :issues
   resources :issue_skills
   resources :projects
   resources :skills
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
