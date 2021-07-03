55fbd6260ce4fad21b384fb2e534a17ad868a35d
TsubasaOura <Tsubasa.Gacct@gmail.com>
2021-07-04 01:30:53 +0900

    [mod]  add seed

--- api/db/fixtures/skill_seed.rb
+++ api/db/fixtures/skill_seed.rb
@@ -1,25 +1,145 @@
 Skill.seed( :id,
 {
   id:1,
+  name: "HTML",
+  status: "frontend",
+  level:3
+},
+{
+  id:2,
+  name: "CSS",
+  status: "frontend",
+  level:3
+},
+{
+  id:3,
+  name: "JavaScript",
+  status: "frontend",
+  level:3
+},
+{
+  id:4,
+  name: "Vue.js",
+  status: "frontend",
+  level: 3
+},
+{
+  id:5,
+  name: "React.js",
+  status: "frontend",
+  level:3
+},
+{
+  id:6,
+  name: "AngularJS",
+  status: "frontend",
+  level:3
+},
+{
+  id:7,
+  name: "jQuery",
+  status: "frontend",
+  level:3
+},
+{
+  id:8,
+  name: "Bootstrap",
+  status: "frontend",
+  level:3
+},
+{
+  id:9,
+  name: "Nuxt.js",
+  status: "frontend",
+  level:3
+},
+{
+  id:10,
+  name: "Next.js",
+  status: "frontend",
+  level:3
+},
+{
+  id:11,
+  name: "C/C++",
+  status: "backend",
+  level:3
+},
+{
+  id:12,
+  name: "C#",
+  status: "backend",
+  level:3
+},
+{
+  id:13,
+  name: "Java",
+  status: "backend",
+  level:3
+},
+{
+  id:14,
+  name: "Python",
+  status: "backend",
+  level: 3
+},
+{
+  id:15,
   name: "Rails",
   status: "backend",
   level: 3
 },
 {
-  id:2,
-  name: "Vue",
-  status: "frontend",
-  level: 3
-},
-{
-  id:3,
-  name: "python",
+  id:16,
+  name: "Go",
   status: "backend",
+  level:3
+},
+{
+  id:17,
+  name: "PHP",
+  status: "backend",
+  level:3
+},
+{
+  id:18,
+  name: "Kotlin",
+  status: "mobile",
   level: 3
 },
 {
-  id:4,
+  id:19,
+  name: "Swift",
+  status: "mobile",
+  level: 3
+},
+{
+  id:20,
+  name: "Flutter",
+  status: "mobile",
+  level: 3
+},
+{
+  id:21,
+  name: "Rust",
+  status: "server",
+  level: 3
+},
+{
+  id:22,
   name: "Nginx",
+  status: "server",
+  level:3
+},
+{
+  id:23,
+  name: "Apache",
+  status: "server",
+  level:3
+},
+{
+  id:24,
+  name: "SQL",
   status: "server",
   level:3
 }
