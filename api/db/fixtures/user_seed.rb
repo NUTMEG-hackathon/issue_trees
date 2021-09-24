42e4c2325213514d3cdca5053184edd6ea9881bd
ryuseifujisaki <fujiryu1122@gmail.com>
2021-09-24 18:38:56 +0900

    change seed data

--- api/db/fixtures/user_seed.rb
+++ api/db/fixtures/user_seed.rb
@@ -1,49 +1,49 @@
 User.seed(:id,
 {
   id:1,
-  name: "gikutaro",
+  name: "技育太郎",
   email: "gikutaro@email.com",
   password: "password"
 },
  {
    id:2,
-   name: "gikugiro",
+   name: "技育次郎",
    email: "gikugiro@email.com",
    password: "password"
  },
  {
   id:3,
-  name: "gikusaburo",
+  name: "技育三郎",
   email: "gikusaburo@email.com",
   password: "password"
  },
  {
   id:4,
-  name: "gikukoziro",
+  name: "技育四郎",
   email: "gikukoziro@email.com",
   password: "password"
  },
  {
   id:5,
-  name: "gikurokurou",
+  name: "技育五郎",
   email: "gikurokurou@email.com",
   password: "password"
  },
  {
   id:6,
-  name: "gikuhanako",
+  name: "技育六郎",
   email: "gikuhanako@email.com",
   password: "password"
  },
  {
   id:7,
-  name: "gikumike",
+  name: "技育マイク",
   email: "gikumike@email.com",
   password: "password"
  },
  {
   id:8,
-  name: "gikuichiro",
+  name: "技育一郎",
   email: "gikuichiro@email.com",
   password: "password"
  },
